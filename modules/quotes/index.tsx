2f628ed867c7bd738791a12fe73d518f35be704d
Oskars Ezerins <oskarsezerins@users.noreply.github.com>
2022-04-10 19:00:14 +0000

    Chore: update barebones quote component

--- modules/quotes/index.tsx
+++ modules/quotes/index.tsx
@@ -2,10 +2,14 @@ import { PROGRAMMING_QUOTES_URL } from 'modules/common/quotesSources'
 import React from 'react'
 import useSWR from 'swr'
 
-const fetcher = (...args: any[]) => fetch(...args).then((res) => res.json())
+const fetcher = (input: RequestInfo, init?: RequestInit | undefined) => fetch(input, init).then((res) => res.json())
 
 function Quotes() {
-  const { data, error } = useSWR(PROGRAMMING_QUOTES_URL, fetcher)
+  const { data, error } = useSWR(PROGRAMMING_QUOTES_URL, fetcher, {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: false,
+  })
 
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
